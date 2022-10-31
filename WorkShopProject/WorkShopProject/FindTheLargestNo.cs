2d04108e7f05b0b18d9dcd3b5853f7829d726205
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-10-31 12:07:01 +0530

    Finding the gretest among three numbers using generics

---
+++ WorkShopProject/WorkShopProject/FindTheLargestNo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShopProject
+{
+    public class FindTheLargestNo
+    { 
+        public static void num<T>(T num1,T num2, T num3) where T : IComparable<T>
+    
+        {
+            if ( num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 )
+            {
+                Console.WriteLine("{0} greater amomg three",num1);
+            }else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                Console.WriteLine("{0} greater amomg three", num2);
+            }
+            else
+             {
+                Console.WriteLine("{0} greater amomg three", num3);
+            }
+        }
+    }
+}
---
+++ WorkShopProject/WorkShopProject/Program.cs
@@ -0,0 +1,4 @@
+﻿using WorkShopProject;
+
+FindTheLargestNo.num(20, 40, 80);
+FindTheLargestNo.num(10.2f, 22.5f, 20.5f);
